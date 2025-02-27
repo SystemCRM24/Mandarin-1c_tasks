80bb4cb17fcd71ab6b36ddeeb50547c9a868053a
Pavel <rossohinpaveldev@gmail.com>
2025-02-27 16:22:07 +0300

    log response

--- src/routers/one_ass.py
+++ src/routers/one_ass.py
@@ -14,12 +14,14 @@ router = APIRouter(tags=['1c'])
 @router.post("/create_task", status_code=200)
 async def create_tasks(order: OrderSchema):
     """Создание задач"""
-    print(order)
     try:
         file_uploader = FileUploader(order.attached_files)
         asyncio.create_task(file_uploader.upload())
         coros = (Task(order, c, file_uploader).put() for c in order.calculation)
-        return await asyncio.gather(*coros)
+        result = await asyncio.gather(*coros)
+        asyncio.create_task(utils.log_schema(order))
+        asyncio.create_task(utils.log_create_task_response(result))
+        return result
     except Exception as e:
         asyncio.create_task(utils.log_exception(e))
         raise exceptions.HTTPException(status_code=500, detail=str(e))
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import logging
 from datetime import datetime
 from pydantic import BaseModel
 import traceback
+import json
 
 
 debug_logger = logging.getLogger("debug_logger")
@@ -11,11 +12,22 @@ debug_logger.handlers = [logging.FileHandler("logs/debug.log", encoding="utf-8")
 separator = '---------- {0} ----------'
 
 
+ct_logger = logging.getLogger('main_logger')
+ct_logger.setLevel(logging.DEBUG)
+ct_logger.handlers = [logging.FileHandler("logs/ct_responses.log", encoding="utf-8")]
+
+
 async def log_exception(e: Exception):
     """Логирование ошибок. Передаём ошибку и дополнительную информацию"""
     debug_logger.info(separator.format(datetime.now()))
     for frame in traceback.format_exception(e):
         debug_logger.info(frame[:-1])
+
+
+async def log_create_task_response(obj):
+    """Логгирует ответ от приложения на создание задачи"""
+    ct_logger.info(separator.format(datetime.now().strftime(r'%Y-%m-%d_%H-%M-%S-%f')))
+    ct_logger.info(json.dumps(obj, indent=4))
 
 
 async def log_schema(schema: BaseModel):
