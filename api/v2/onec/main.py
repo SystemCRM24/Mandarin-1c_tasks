fa826c144ad09c58924885b08f536c07699a71c9
Pavel <rossohinpaveldev@gmail.com>
2025-03-06 16:45:43 +0300

    logs

--- api/v2/onec/main.py
+++ api/v2/onec/main.py
@@ -8,6 +8,6 @@ router = APIRouter(prefix='/onec')
 
 @router.post('', status_code=200)
 async def process_order(order: Dict[str, Any]):
-    with open('sample.json', 'w') as file:
+    with open('logs/sample.json', 'w') as file:
         json.dump(order, file)
     return {'message': 'success'}
