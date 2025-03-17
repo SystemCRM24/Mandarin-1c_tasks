56ca1b286137c8bea45abf76449c906816c3c70c
Pavel <rossohinpaveldev@gmail.com>
2025-03-17 16:29:25 +0300

    next_date

--- api/v2/tests/main.py
+++ api/v2/tests/main.py
@@ -1,28 +1,9 @@
-import aiocache
-import asyncio
+import random
+
+d = lambda x: x != 9
+n = random.randint(0, 10)
 
 
-decorator = aiocache.cached(namespace='test')
-
-
-@decorator
-async def get_some_dict(some_arg = None):
-    print('calculated')
-    return {'1': 1, 'some_arg': some_arg}
-
-
-async def main():
-    await get_some_dict()
-    await get_some_dict(1)
-    k = decorator.get_cache_key(get_some_dict, tuple(), {})
-    await decorator.cache.delete(k)
-    r = await decorator.cache.exists(key=k)
-    print(r)
-    # print(await cache.exists(None, 'test'))
-    # d1 = await cache.get(key=get_some_dict.cache_key(None), namespace='test')
-    # d2 = await cache.get(key=get_some_dict.cache_key(None), namespace='test')
-    # print(d1)
-    # print(d2)
-
-
-asyncio.run(main())
+while d(n):
+    print(n)
+    n = random.randint(0, 10)
