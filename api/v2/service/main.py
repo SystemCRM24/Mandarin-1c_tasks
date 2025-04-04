ba7009b146bfece33722fc073714f04f09397573
Pavel <rossohinpaveldev@gmail.com>
2025-04-04 15:39:22 +0300

    json from request

--- api/v2/service/main.py
+++ api/v2/service/main.py
@@ -1,6 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
-import json
 import base64
 
 from .cat import html_cat
@@ -23,9 +22,9 @@ async def trigger_ws_event() -> str:
 
 
 @router.post('/upload_json', status_code=200)
-async def upload_json(data: dict):
+async def upload_json(request: Request):
     """Загружает произвольный json-файл на диск битрикса"""
-    json_string = json.dumps(data, ensure_ascii=True, indent=2)
-    b64_binary = base64.b64encode(json_string)
+    json_bytes = await request.body()
+    b64_binary = base64.b64encode(json_bytes)
     b64_message = b64_binary.decode('utf-8')
     await upload_file('request.json', b64_message)
