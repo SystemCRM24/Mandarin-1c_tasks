a7e05f893147b9bf6e22d645c28921858360ac2c
Pavel <rossohinpaveldev@gmail.com>
2025-04-11 17:48:17 +0300

    model dumb json

--- api/v3/service/main.py
+++ api/v3/service/main.py
@@ -1,20 +1,26 @@
 from datetime import datetime
-import json
 from api.utils import SERVER_TZ
 
 from api.v3.schemas.onec import ResponseSchema, OrderSchema
+from pydantic import BaseModel
+
+
+class LogSchema(BaseModel):
+    time: datetime
+    request: OrderSchema
+    response: ResponseSchema
 
 
 async def log_onec_request(request: OrderSchema, response: ResponseSchema):
     """Логгирует запрос и ответ на него по задаче из 1с"""
     now = datetime.now(SERVER_TZ)
-    request = request.model_dump()
-    for file_item in request.get('files'):
-        file_item['binary'] = file_item.get('binary', '')[:40] + '(...)'
-    dct = {
-        'time': now.isoformat(),
-        'request': request,
-        'response': response.model_dump()
-    }
-    with open(f'logs/{now.strftime(r'%Y-%m-%d_%H-%M-%S')}_{request['name']}.json', 'w') as file:
-        json.dump(dct, file, ensure_ascii=True, indent=2)
+    request = request.model_copy(deep=True)
+    for file_item in request.files:
+        file_item.binary = file_item.binary[:40] + '(...)'
+    log = LogSchema(
+        time=now,
+        request=request,
+        response=response
+    )
+    with open(f'logs/{now.strftime(r'%Y-%m-%d_%H-%M-%S')}_{request.name}.json', 'w') as file:
+        file.write(log.model_dump_json())
